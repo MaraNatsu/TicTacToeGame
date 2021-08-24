702acebed33675c184e4cd04393d7baab79d74d7
MaraNatsu <ritamaniuk@ukr.net>
2021-08-25 00:25:09 +0300

    Second player bot was added

--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -20,8 +21,9 @@ public class PlayerColor
 
 public class GameControllerScript : MonoBehaviour
 {
-    private string _playerCellText;
+    private string _playerText;
     private int _moveCount;
+    private string _botText;
 
     public Text[] GridCellsList;
     public GameObject GameOverPanel;
@@ -57,19 +59,20 @@ public class GameControllerScript : MonoBehaviour
         SideChoice.SetActive(false);
     }
 
-    private void ChangePlayer()
-    {
-        _playerCellText = (_playerCellText == "X") ? "O" : "X";
+    // A function for 2 active players
+    //private void ChangePlayer()
+    //{
+    //    _playerCellText = (_playerCellText == "X") ? "O" : "X";
 
-        if (_playerCellText == "X")
-        {
-            SetPlayerColors(PlayerX, PlayerO);
-        }
-        else
-        {
-            SetPlayerColors(PlayerO, PlayerX);
-        }
-    }
+    //    if (_playerCellText == "X")
+    //    {
+    //        SetPlayerColors(PlayerX, PlayerO);
+    //    }
+    //    else
+    //    {
+    //        SetPlayerColors(PlayerO, PlayerX);
+    //    }
+    //}
 
     private void GameOver(string gameResult)
     {
@@ -82,7 +85,7 @@ public class GameControllerScript : MonoBehaviour
         }
         else
         {
-            SetGameOverText($"{_playerCellText} Wins!");
+            SetGameOverText($"{gameResult} Wins!");
         }
 
         RestartButton.SetActive(true);
@@ -102,12 +105,12 @@ public class GameControllerScript : MonoBehaviour
         previousPlayer.PanelText.color = InactivePlayerColor.TextColor;
     }
 
-    private bool CheckThreeInRow(int cellIndex1, int cellIndex2, int cellIndex3)
+    private bool CheckThreeInRow(int cellIndex1, int cellIndex2, int cellIndex3, string playerText)
     {
         if (
-            GridCellsList[cellIndex1].text == _playerCellText &&
-            GridCellsList[cellIndex2].text == _playerCellText &&
-            GridCellsList[cellIndex3].text == _playerCellText
+            GridCellsList[cellIndex1].text == playerText &&
+            GridCellsList[cellIndex2].text == playerText &&
+            GridCellsList[cellIndex3].text == playerText
             )
         {
             return true;
@@ -138,17 +141,61 @@ public class GameControllerScript : MonoBehaviour
         PlayerO.PanelText.color = InactivePlayerColor.TextColor;
     }
 
+    private void CheckWins(string playerText)
+    {
+        _moveCount++;
+
+        if (CheckThreeInRow(0, 1, 2, playerText))
+        {
+            GameOver(playerText);
+        }
+        else if (CheckThreeInRow(3, 4, 5, playerText))
+        {
+            GameOver(playerText);
+        }
+        else if (CheckThreeInRow(6, 7, 8, playerText))
+        {
+            GameOver(playerText);
+        }
+        else if (CheckThreeInRow(0, 3, 6, playerText))
+        {
+            GameOver(playerText);
+        }
+        else if (CheckThreeInRow(1, 4, 7, playerText))
+        {
+            GameOver(playerText);
+        }
+        else if (CheckThreeInRow(2, 5, 8, playerText))
+        {
+            GameOver(playerText);
+        }
+        else if (CheckThreeInRow(0, 4, 8, playerText))
+        {
+            GameOver(playerText);
+        }
+        else if (CheckThreeInRow(2, 4, 6, playerText))
+        {
+            GameOver(playerText);
+        }
+        else if (_moveCount >= 9)
+        {
+            GameOver("draw");
+        }
+    }
+
     public void SetStartingPlayer(string startingPlayer)
     {
-        _playerCellText = startingPlayer;
+        _playerText = startingPlayer;
 
-        if (_playerCellText == "X")
+        if (_playerText == "X")
         {
             SetPlayerColors(PlayerX, PlayerO);
+            _botText = PlayerO.PanelText.text;
         }
         else
         {
             SetPlayerColors(PlayerO, PlayerX);
+            _botText = PlayerX.PanelText.text;
         }
 
         StarGame();
@@ -156,53 +203,35 @@ public class GameControllerScript : MonoBehaviour
 
     public string GetPlayerTurn()
     {
-        return _playerCellText;
+        return _playerText;
     }
 
     public void EndTurn()
     {
-        _moveCount++;
+        CheckWins(_playerText);
 
-        if (CheckThreeInRow(0, 1, 2))
+        if (_moveCount < 9)
         {
-            GameOver(_playerCellText);
+            MakeMoveOfBot();
+            CheckWins(_botText);
         }
-        else if (CheckThreeInRow(3, 4, 5))
+    }
+
+    public void MakeMoveOfBot()
+    {
+        List<Text> availableCells = new List<Text>();
+
+        for (int i = 0; i < GridCellsList.Length; i++)
         {
-            GameOver(_playerCellText);
+            if (GridCellsList[i].text == "")
+            {
+                availableCells.Add(GridCellsList[i]);
+            }
         }
-        else if (CheckThreeInRow(6, 7, 8))
-        {
-            GameOver(_playerCellText);
-        }
-        else if (CheckThreeInRow(0, 3, 6))
-        {
-            GameOver(_playerCellText);
-        }
-        else if (CheckThreeInRow(1, 4, 7))
-        {
-            GameOver(_playerCellText);
-        }
-        else if (CheckThreeInRow(2, 5, 8))
-        {
-            GameOver(_playerCellText);
-        }
-        else if (CheckThreeInRow(0, 4, 8))
-        {
-            GameOver(_playerCellText);
-        }
-        else if (CheckThreeInRow(2, 4, 6))
-        {
-            GameOver(_playerCellText);
-        }
-        else if (_moveCount >= 9)
-        {
-            GameOver("draw");
-        }
-        else
-        {
-            ChangePlayer();
-        }
+
+        int cellToTake = Random.Range(0, availableCells.Count);
+        availableCells[cellToTake].text = _botText;
+        availableCells[cellToTake].GetComponentInParent<Button>().interactable = false;
     }
 
     public void RestartGame()
